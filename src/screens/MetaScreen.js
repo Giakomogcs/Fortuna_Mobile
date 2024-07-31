88369b9d81f824d43f4ec6508ced1ea97d92cd91
GaEstevam <estevam.gabriel@hotmail.com>
2024-07-31 11:22:09 -0300

    MetaScreen

--- src/screens/MetaScreen.js
+++ src/screens/MetaScreen.js
@@ -1,224 +1,200 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert, Platform } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { TokenContext } from '../components/TokenContext'; // Importar o contexto do token
+import React, { useState } from 'react';
+import { View, Text, TextInput, Switch, StyleSheet, Button, Alert, ScrollView } from 'react-native';
 
-const SignupScreen = ({ navigation }) => {
+const MetaScreen = () => {
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [confirmEmail, setConfirmEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [birthDate, setBirthDate] = useState(new Date());
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [secureTextEntry, setSecureTextEntry] = useState(true);
-  const [loading, setLoading] = useState(false);
+  const [typeGoal, setTypeGoal] = useState(false);
+  const [patrimony, setPatrimony] = useState();
+  const [initialPatrimony, setInitialPatrimony] = useState();
+  const [timeDesired, setTimeDesired] = useState(); // anos
+  const [monthlyAport, setMonthlyAport] = useState();
+  const [rate, setRate] = useState(0.01);
+  const [dividends, setDividends] = useState();
   const [responseMessage, setResponseMessage] = useState('');
-  const { setToken } = useContext(TokenContext); // Usar o contexto do token
 
-  const handleRegister = async () => {
-    setLoading(true);
+  const handleSubmit = async () => {
+    const goalData = {
+      name,
+      patrimony: typeGoal ? 0 : patrimony,
+      dividends: typeGoal ? dividends : 0,
+      type_goal: typeGoal,
+      initial_patrimony: typeGoal ? 0 : initialPatrimony,
+      time_desired: timeDesired,
+      monthly_aport: monthlyAport,
+      rate: rate,
+    };
+
     try {
-      const formattedBirthDate = birthDate.toISOString();
-
-      const registerResponse = await fetch('https://fortuna-api.onrender.com/api/users', {
+      // Authenticate and get the token
+      const authResponse = await fetch('https://fortuna-api.onrender.com/api/sessions', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name,
-          email,
-          password,
-          birthDate: formattedBirthDate,
+          email: 'neguinho@gmail.com',
+          password: '123',
         }),
       });
 
-      const registerData = await registerResponse.json();
-
-      if (!registerResponse.ok) {
-        setResponseMessage(registerData.message || 'Failed to register.');
-        setLoading(false);
+      if (!authResponse.ok) {
+        setResponseMessage('Failed to authenticate.');
         return;
       }
 
-      const token = registerData.token;
-      setToken(token); // Armazenar o token no contexto
-      setResponseMessage('Cadastro realizado com sucesso.');
-      Alert.alert('Sucesso', 'Cadastro realizado com sucesso.');
-      navigation.navigate('FinancialGoals'); // Redirecionar para FinancialGoalsScreen
+      const authData = await authResponse.json();
+      const token = authData.token;
+
+      // Send the goal data with the token
+      const response = await fetch('https://fortuna-api.onrender.com/api/goals/preview', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(goalData),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setResponseMessage(JSON.stringify(result, null, 2));
+      } else {
+        setResponseMessage('Failed to submit goal data.');
+      }
     } catch (error) {
-      setResponseMessage('Ocorreu um erro.');
-      Alert.alert('Erro', 'Ocorreu um erro.');
-    } finally {
-      setLoading(false);
+      setResponseMessage('An error occurred while submitting goal data.');
+      console.error('Error:', error);
     }
-  };
-
-  const onChangeDate = (event, selectedDate) => {
-    setShowDatePicker(false);
-    if (selectedDate) {
-      setBirthDate(selectedDate);
-    }
-  };
-
-  const showDatepicker = () => {
-    setShowDatePicker(true);
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.formContainer}>
-        <Text style={styles.headerText}>
-          Crie sua conta, <Text style={styles.headerTextBold}>e aproveite nosso app. Estamos te esperando</Text>
-        </Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Seu nome"
-          value={name}
-          onChangeText={setName}
+      <Text style={styles.label}>Name:</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+
+      <View style={styles.switchContainer}>
+        <Text style={styles.label}>Type Goal:</Text>
+        <Switch
+          value={typeGoal}
+          onValueChange={setTypeGoal}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="Seu email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Confirme seu email"
-          value={confirmEmail}
-          onChangeText={setConfirmEmail}
-        />
-        <View style={styles.passwordContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Sua senha"
-            secureTextEntry={secureTextEntry}
-            value={password}
-            onChangeText={setPassword}
-          />
-          <TouchableOpacity onPress={() => setSecureTextEntry(!secureTextEntry)}>
-            <MaterialIcons name={secureTextEntry ? 'visibility-off' : 'visibility'} size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.passwordContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Confirme sua senha"
-            secureTextEntry={secureTextEntry}
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-          />
-          <TouchableOpacity onPress={() => setSecureTextEntry(!secureTextEntry)}>
-            <MaterialIcons name={secureTextEntry ? 'visibility-off' : 'visibility'} size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-        {Platform.OS === 'web' ? (
-          <View>
-            <Text style={styles.datePickerLabel}>Data de nascimento</Text>
-            <input
-              type="date"
-              value={birthDate.toISOString().split('T')[0]}
-              onChange={(e) => setBirthDate(new Date(e.target.value))}
-              style={styles.input}
-            />
-          </View>
-        ) : (
-          <TouchableOpacity onPress={showDatepicker}>
-            <TextInput
-              style={styles.input}
-              placeholder="Data de nascimento"
-              value={birthDate.toISOString().split('T')[0]}
-              editable={false}
-            />
-          </TouchableOpacity>
-        )}
-        {showDatePicker && (
-          <DateTimePicker
-            value={birthDate}
-            mode="date"
-            display="default"
-            onChange={onChangeDate}
-          />
-        )}
-        <TouchableOpacity style={styles.button} onPress={handleRegister} disabled={loading}>
-          <Text style={styles.buttonText}>Criar sua conta</Text>
-        </TouchableOpacity>
-        {responseMessage ? (
-          <Text style={styles.responseMessage}>{responseMessage}</Text>
-        ) : null}
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.loginText}>Você já tem uma conta? <Text style={styles.loginTextBold}>Logar!</Text></Text>
-        </TouchableOpacity>
       </View>
+
+      {!typeGoal && (
+        <>
+          <Text style={styles.label}>Patrimony:</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            value={String(patrimony)}
+            onChangeText={text => setPatrimony(Number(text))}
+          />
+
+          <Text style={styles.label}>Initial Patrimony:</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            value={String(initialPatrimony)}
+            onChangeText={text => setInitialPatrimony(Number(text))}
+          />
+        </>
+      )}
+
+      {typeGoal && (
+        <>
+          <Text style={styles.label}>Dividends:</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            value={String(dividends)}
+            onChangeText={text => setDividends(Number(text))}
+          />
+
+          <Text style={styles.label}>Initial Patrimony:</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            value={String(initialPatrimony)}
+            onChangeText={text => setInitialPatrimony(0)}
+            editable={false} // Desabilita edição
+          />
+        </>
+      )}
+
+      <Text style={styles.label}>Time Desired (years):</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={String(timeDesired)}
+        onChangeText={text => setTimeDesired(Number(text))}
+      />
+
+      <Text style={styles.label}>Monthly Aport:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={String(monthlyAport)}
+        onChangeText={text => setMonthlyAport(Number(text))}
+      />
+
+      <Text style={styles.label}>Rate:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={String(rate)}
+        onChangeText={text => setRate(Number(text))}
+      />
+
+      <Button title="Submit" onPress={handleSubmit} />
+
+      {responseMessage ? (
+        <View style={styles.responseContainer}>
+          <Text style={styles.responseLabel}>Response:</Text>
+          <Text style={styles.responseText}>{responseMessage}</Text>
+        </View>
+      ) : null}
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: '#fff',
+    padding: 16,
   },
-  formContainer: {
-    marginTop: '40%',
-    padding: 20,
-  },
-  headerText: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  headerTextBold: {
+  label: {
+    marginVertical: 8,
     fontWeight: 'bold',
   },
   input: {
+    height: 40,
+    borderColor: 'gray',
     borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 10,
-    flex: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
   },
-  datePickerLabel: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  passwordContainer: {
+  switchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 8,
+    marginBottom: 12,
+  },
+  responseContainer: {
+    marginTop: 20,
     padding: 10,
-    marginBottom: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
   },
-  button: {
-    backgroundColor: '#8A2BE2',
-    paddingVertical: 15,
-    borderRadius: 8,
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  responseMessage: {
-    textAlign: 'center',
-    color: 'red',
-    marginBottom: 20,
-  },
-  loginText: {
-    textAlign: 'center',
-    color: '#000',
-  },
-  loginTextBold: {
-    color: '#8A2BE2',
+  responseLabel: {
     fontWeight: 'bold',
+  },
+  responseText: {
+    marginTop: 5,
+    color: '#333',
   },
 });
 
-export default SignupScreen;
+export default MetaScreen;
